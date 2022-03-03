85140e690f1e30d3d3303c136163db551f63141a
abigcatc683 <abigcatc683@SDS-L-763HC63>
2022-03-03 14:43:16 -0500

    added sound for print; prints line by line; added comments

--- cashRegister/Form1.cs
+++ cashRegister/Form1.cs
@@ -7,15 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Media;
 
 namespace cashRegister
 {
     public partial class Form1 : Form
     {
+        //global variables
         double redShoes;
         double blueShoes;
         double greyShoes;
 
+        //cost of different types
         double redShoesCost = 20;
         double blueShoesCost = 25;
         double greyShoesCost = 15;
@@ -40,16 +44,19 @@ namespace cashRegister
                 blueShoes = Convert.ToDouble(blueShoesInput.Text);
                 greyShoes = Convert.ToDouble(greyShoesInput.Text);
 
+                //calculate subtotal, tax amount, and total
                 subTotal = (redShoes * redShoesCost) + (blueShoes * blueShoesCost) + (greyShoes * greyShoesCost);
 
                 taxAmount = subTotal * taxRate;
                 total = subTotal + taxAmount;
 
+                //outputing the totals and tax amount
                 subTotalOutput.Text = $"{subTotal.ToString("C")}";
                 taxOutput.Text = $"{taxAmount.ToString("C")}";
                 totalOutput.Text = $"{total.ToString("C")}";
             }
             catch {
+                //Prints "ERROR" if letter (not a number) is the input
                 subTotalOutput.Text = "ERROR";
                 taxOutput.Text = "ERROR";
                 totalOutput.Text = "ERROR";
@@ -62,8 +69,11 @@ namespace cashRegister
             {
                 amountTendered = Convert.ToInt16(tenderedInput.Text);
 
+                //calculate change
                 change = amountTendered - total;
                 changeOutput.Text = change.ToString("C");
+
+                //can't press print receipt button until accepted amount is in
                 if (change >= 0)
                 {
                     printReceiptButton.Enabled = true;
@@ -73,36 +83,69 @@ namespace cashRegister
                     changeOutput.Text = "Not Enough Money";
                 }
             }
-            catch
-            {
+            catch {
+                //if something other than a number is the input, prints "ERROR"
                 changeOutput.Text = "ERROR";
             }
         }
 
         private void printReceiptButton_Click(object sender, EventArgs e)
         {
+            //printing sound
+            SoundPlayer player = new SoundPlayer(Properties.Resources.receiptPrintSound);
+
+            player.Play();
+
+            //thread.sleep and refresh() wait half a second before printing next line
             receiptLabel.Text = "\n            SHOES SHOES SHOES!!!";
+            Thread.Sleep(200);
+            Refresh();
 
             receiptLabel.Text += "\n\nOrder Number 4023";
+            Thread.Sleep(200);
+            Refresh();
             receiptLabel.Text += "\nMarch 1, 2022";
+            Thread.Sleep(200);
+            Refresh();
 
+            //prints number of shoes bought and price for 1
             receiptLabel.Text += $"\n\nRed Shoes  x{redShoes} @   {redShoesCost.ToString("C")}";
+            Thread.Sleep(200);
+            Refresh();
             receiptLabel.Text += $"\nBlue Shoes  x{blueShoes} @  {blueShoesCost.ToString("C")}";
+            Thread.Sleep(200);
+            Refresh();
             receiptLabel.Text += $"\nGrey Shoes  x{greyShoes} @ {greyShoesCost.ToString("C")}";
+            Thread.Sleep(200);
+            Refresh();
 
+            //prints subtotal, tax amount, and total
             receiptLabel.Text += $"\n\nSubtotal                 {subTotal.ToString("C")}";
+            Thread.Sleep(200);
+            Refresh();
             receiptLabel.Text += $"\nTax                            {taxAmount.ToString("C")}";
+            Thread.Sleep(200);
+            Refresh();
             receiptLabel.Text += $"\nTotal                       {total.ToString("C")}";
+            Thread.Sleep(200);
+            Refresh();
 
+            //prints tendered and change amount
             receiptLabel.Text += $"\n\nTendered              {amountTendered.ToString("C")}";
+            Thread.Sleep(200);
+            Refresh();
             receiptLabel.Text += $"\nChange                    {change.ToString("C")}";
+            Thread.Sleep(200);
+            Refresh();
 
             receiptLabel.Text += "\n\nHave a nice day!";
+            Thread.Sleep(200);
+            Refresh();
         }
 
         private void newOrderButton_Click(object sender, EventArgs e)
         {
-
+            //resets button so cannot print another reciept unless inputs are put in again
             printReceiptButton.Enabled = false;
 
             redShoesInput.Text = String.Empty;
@@ -119,6 +162,7 @@ namespace cashRegister
 
             receiptLabel.Text = String.Empty;
 
+            //resets the variables
             redShoes = 0;
             blueShoes = 0;
             greyShoes = 0;
